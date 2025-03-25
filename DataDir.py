d83105f9c8d74d74940d379a98c30cf509fd7966
percocet69 <kedar.malshe@gmail.com>
2025-03-25 21:31:19 +0530

    Update DataDir.py

--- DataDir.py
+++ DataDir.py
@@ -8,6 +8,7 @@ match_innings_dir = 'SplitData/match_innings_data.csv'
 match_data_dir = 'SplitData/match_data.csv'
 batsman_stats_dir = 'SplitData/batsman_stats.csv'
 batsman_test_stats_dir = 'SplitTestData/batsman_stats.csv'
+matchup_stats_dir = 'SplitData/batsman_vs_bowler_stats.csv'
 
 # IndicatorFiles
 indicator_city_dir = 'IndicatorData/city.csv'
@@ -18,3 +19,4 @@ indicator_match_type_dir = 'IndicatorData/match_type.csv'
 indicator_team_type_dir = 'IndicatorData/team_type.csv'
 indicator_teams_dir = 'IndicatorData/teams.csv'
 indicator_players_dir = 'IndicatorData/players.csv'
+indicator_wicket_type_dir = 'IndicatorData/wicket_type.csv'
