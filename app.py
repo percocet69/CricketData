7b6848ff750ce74dd41d5d65c17ef663651cbc86
percocet69 <kedar.malshe@gmail.com>
2025-03-25 22:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from screens.innings_page import show_innings_page
 from screens.summary_page import show_summary_page
 from screens.batsman_page import show_batsman_page
 from screens.matchup_page import show_matchup_page
+from screens.readme_page import show_readme_page
 
 
 st.set_page_config(page_title="Cricket Match Viewer", layout="wide")
@@ -13,6 +14,8 @@ if "view_section" not in st.session_state:
 
 with st.sidebar:
     st.markdown("### Select Dataset")
+    if st.button("ReadMe / Project Info"):
+        st.session_state.view_section = "readme"
     if st.button("Match Summary Data"):
         st.session_state.view_section = "summary"
     # if st.button("Match Innings Data"):
@@ -30,3 +33,5 @@ elif st.session_state.view_section == "batsman":
     show_batsman_page()
 elif st.session_state.view_section == "matchup":
     show_matchup_page()
+elif st.session_state.view_section == "readme":
+    show_readme_page()
